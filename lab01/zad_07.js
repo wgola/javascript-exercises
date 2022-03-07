eb35e279157ebe5b6e616a5204a42f13a74e89d4
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-07 19:17:49 +0100

    Laboratoria 1 - zadanie 7

--- lab01/zad_07.js
+++ lab01/zad_07.js
@@ -1,4 +1,4 @@
-const tablica = [1, 5, 6, 5, 5, 1, 5];
+const tablica = [1, 5, 6, 5, 5, 1, 5, 7, 7, 8, 6];
 
 const ile = [];
 
@@ -18,4 +18,29 @@ for (let i = 0; i < tablica.length; i++) {
         odp = " raz";
     }
     console.log(tablica[i] + " występuje " + ile[i] + odp)
+}
+
+console.log();
+
+const wyswietlone = [];
+
+let wolne = 0;
+
+for (let i = 0; i < tablica.length; i++) {
+    let czy_bylo = false;
+    for (let j = 0; j < wyswietlone.length; j++) {
+        if (wyswietlone[j] == tablica[i]) {
+            czy_bylo = true;
+            break;
+        }
+    }
+    if (!czy_bylo) {
+        wyswietlone[wolne] = tablica[i];
+        let odp = " razy";
+        if (ile[i] == 1) {
+            odp = " raz";
+        }
+        console.log(tablica[i] + " występuje " + ile[i] + odp)
+        wolne++;
+    }
 }
