42883ac0fbd4aff4a1b3b1d77d6ccbf0ba8e14c1
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-07 13:25:24 +0100

    Laboratoria 1 - zadanie 5

--- lab01/zad_03.js
+++ lab01/zad_03.js
@@ -1,34 +1,34 @@
-let a = [-4321, 4232, 3213, 54, 125, 436, -437, 128, 49, 1510, -411];
+const b = [-4321, 4232, 3213, 54, 125, 436, -437, 128, 49, 1510, -411];
 
 console.log("Elementy tablicy: ")
 
-for (let i = 0; i < a.length; i++) {
-    console.log(a[i]);
+for (let i = 0; i < b.length; i++) {
+    console.log(b[i]);
 }
 
-let min = a[0];
+let min = b[0];
 
-for (let i = 0; i < a.length; i++) {
-    if (a[i] < min) {
-        min = a[i];
+for (let i = 0; i < b.length; i++) {
+    if (b[i] < min) {
+        min = b[i];
     }
 }
 
 console.log("Wartość najmniejsza: " + min);
 
 
-// let tmp = a[0];
-// a[0] = a[a.length-1];
-// a[a.length-1] = tmp;    
+// let tmp = b[0];
+// b[0] = b[b.length-1];
+// b[b.length-1] = tmp;    
 
-for (let i = 0; i < a.length/2; i++) {
-    let tmp = a[i];
-    a[i] = a[a.length-1-i];
-    a[a.length-1-i] = tmp;    
+for (let i = 0; i < b.length/2; i++) {
+    let tmp = b[i];
+    b[i] = b[b.length-1-i];
+    b[b.length-1-i] = tmp;    
 }
 
 console.log("Odwrócona tablica: ");
 
-for (let i = 0; i < a.length; i++) {
-    console.log(a[i]);
+for (let i = 0; i < b.length; i++) {
+    console.log(b[i]);
 }
