e97b4646883054239f5c7ad6a202b295c9eb38e4
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-07 12:52:08 +0100

    Laboratoria 1 - zadanie 2

---
+++ lab01/zad_02.js
@@ -0,0 +1,15 @@
+let a = 12;
+
+if (a < 0) {
+    console.log("Liczba jest ujemna");
+} else if (a == 0) {
+    console.log("Liczba jest równa 0");
+} else {
+    console.log("Liczba jest dodatnia");
+}
+
+if (a % 2 == 0) {
+    console.log("Liczba jest parzysta");
+} else {
+    console.log("Liczba jest nieparzysta");
+}
