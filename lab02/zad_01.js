46a4330a8ce9df55b1b52e3bc4f68d3eeeb5aa2f
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-14 12:45:05 +0100

    Laboratoria 2 - zadanie 1

--- lab02/zad_01.js
+++ lab02/zad_01.js
@@ -1,1 +1,23 @@
 'use strict';
+const numbers = [4, 3, 5, 1, 3, 2, 21, 1, 65, -43, 9];
+
+const unique = [];
+
+let wolne_miejsce = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    let czy_wystapilo = false;
+    for (let j = 0; j < unique.length; j++) {
+        if (unique[j] === numbers[i]) {
+            czy_wystapilo = true;
+            break;
+        }
+    }
+    if (!czy_wystapilo) {
+        unique[wolne_miejsce] = numbers[i];
+        wolne_miejsce++;
+    } 
+}
+
+console.log(numbers);
+console.log(unique);
