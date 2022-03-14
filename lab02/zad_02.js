950f350c24fa1158666d915d59a654954b0f3843
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-03-14 13:09:16 +0100

    Laboratoria 2 - zadanie 2

--- lab02/zad_02.js
+++ lab02/zad_02.js
@@ -4,12 +4,14 @@ const array = [4, 2, 'a', 'b', 3, 'aa', 'ww', 2, 'ab', -2];
 let wolne_miejsce = 0;
 
 console.log(array);
-for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-        if (typeof array[j] !== "number") {
-            let tmp = array[array.length-1];
-            array[array.length-1] = array[j];
-            array[j] = tmp;
+for (let g = 0; g < array.length; g++) {
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] !== "number") {
+            for (let j = i; j < array.length-1; j++) {
+                let tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+            }    
         }
     }
 }
