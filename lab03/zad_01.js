093931dedefdee8dbd73fc0e96ce06d6fcc511b1
Wojciech Gola <wgola@INF.UG.EDU.PL>
2022-03-28 12:29:37 +0200

    Laboratoria 3 - zadanie 1

---
+++ lab03/zad_01.js
@@ -0,0 +1,155 @@
+﻿'use strict';
+
+// Poniższe fragmenty kodu zostały zakomentowane w celu utrzymania porządku. 
+// Odkomentowuj je na bieżąco i zapisuj odpowiedzi w komentarzu. 
+// Ostatecznie przed wrzuceniem pliku na repozytorium zakomentowane powinny być tylko dodane odpowiedzi i fragmenty kodu powodujące ewentualne błędy.
+
+// ========================= Zadanie 1 =========================
+// Co zwróci funkcja poniższa funkcja w każdym z poniższych przypadków?
+// Wyjaśnij, dlaczego w niektórych przypadkach wyniki różnią się.
+
+// ========================== UWAGA =============================
+// Zapis 
+// (impression) ? console.log('A') : console.log('B');
+// Jest skróconą wersją:
+// if (impression) {
+//     console.log('A');
+// } else {
+//     console.log('B');
+// }
+// ==============================================================
+
+function isEquals(val_1, val_2) {
+    (val_1 == val_2) ? console.log('A') : console.log('B');
+    (val_1 === val_2) ? console.log('C') : console.log('D');
+}
+
+// isEquals(2, '2');
+// isEquals(null, undefined);
+// isEquals(undefined, NaN);
+// isEquals(['a', 'b', 'c'], ['b', 'c', 'd']);
+// isEquals(0, '');
+// isEquals('0', '');
+// isEquals(+0, -0);
+// isEquals(0, false);
+// isEquals(0, 'false');
+// isEquals([1, 2], '1,2');
+
+!!false;
+!!true;
+!!undefined;
+!!null;
+
+// ========================= Zadanie 2 =========================
+// Jaki będzie efekt działania poniższego fragmentu kodu?
+// Wyjaśnij wynik
+
+const person = {
+    firstName: 'Jan',
+    lastName: 'Kowalski'
+}
+
+// console.log(person);
+// person = {};
+// console.log(person);
+
+// ========================= Zadanie 3 =========================
+// Co zostanie wyświetlone na ekranie?
+// Wyjaśnij wynik
+
+// let number = 3;
+// console.log(number); {
+//     let number = 4;
+//     console.log(number);
+// }
+// console.log(number);
+
+// ========================= Zadanie 4 =========================
+// Czym się różnią poniższe dwa fragmenty kodu?
+// Jak działa operator '...'?
+
+// const arr = [1, 2];
+// const newArr1 = [arr, 3, 4];
+// console.log(newArr1);
+// const newArr2 = [...arr, 3, 4];
+// console.log(newArr2);
+
+// Co zostanie wyświetlone na ekranie?
+// Wyjaśnij wynik
+
+// const word = 'javascript';
+// const arrWord = [...word];
+// console.log(arrWord);
+
+// ========================= Zadanie 5 =========================
+// Zapoznaj się z kodem poniżej. Jaki będzie jego wynik i dlaczego?
+
+// var hello = 'Hello world!';
+// var result = hello / 2;
+
+// result;
+
+// Number.isNaN(result);
+// Number.isNaN(hello);
+
+// ========================= Zadanie 6 =========================
+// Zapoznaj się z przykładami poniżej. Jaka jest różnica między var a let/const?
+
+// var car = 'BMW';
+
+// function showCar() {
+//     car = 'Audi';
+//     model = 'A5';
+//     console.log('Great car!');
+// }
+
+// showCar();
+
+// car;
+// model;
+
+// -------
+
+// var name = 'Bryan';
+
+// (function differentName() {
+//     var name = 'Adam';
+//     console.log(name);
+// })();
+
+// console.log(name);
+
+// -------
+
+// if (true) {
+//     var a = 2;
+// }
+// console.log(a);
+
+// if (true) {
+//     const b = 2;
+// }
+// console.log(b);
+
+// -------
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// -------
+
+// var test = "var1";
+// var test = "var2";
+
+// let test2 = "let1";
+// let test2 = "let2";
+
+// ========================= Zadanie 7 =========================
+// Do czego używany jest 'use strict' w pierwszej linijce skryptu?
