de033d6215d1279f0772780051649f957447f77f
Wojciech Gola <wgola@INF.UG.EDU.PL>
2022-03-28 13:36:46 +0200

    Laboratoria 3 - zadanie 4

---
+++ lab03/zad_04.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function ileRazy(tablica) {
+    const wynik = []
+    for (let i = 0; i < tablica.length; i++) {
+
+        for (let j = 0; j < wynik.length; j++) {
+             
+        }
+    }
+}
