cbf62246468916fc8a7f4ed8492b68332317b50d
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-04-11 12:38:58 +0200

    Laboratoria 6 - zadanie 2

--- lab06/src/index.js
+++ lab06/src/index.js
@@ -1,6 +1,11 @@
+const runFunc = () => console.log('Funkcja 1');
+
+const runFunc2 = () => console.log('Funkcja 2');
+
 switch(status) {
     case 'Completed':
         runFunc();
+        break;
  
     case 'Running': 
         runFunc2();
