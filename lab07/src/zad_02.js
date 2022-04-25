4a83d80cfd86c79e5063f4997020df251d234bb9
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-04-25 19:16:58 +0200

    Laboratoria 7 - zadanie 2

--- lab07/src/zad_02.js
+++ lab07/src/zad_02.js
@@ -59,22 +59,73 @@ czyLiczby([1, 2, 3]);
 
 // 6.
 const losowo = (n, min, max) => {
-    for (let i = 0; i < n; i++) {
-        const losowa = lodash.random(min, max);
-        lodash.forEach(lp3, elem => {
-            if (elem.place === losowa){
-                console.log(elem);
-            }
-        })
+    const pom = (x) => {
+        if (x < n) {
+            const losowa = lodash.random(min, max);
+            lodash.forEach(lp3, elem => {
+                if (elem.place === losowa){
+                    console.log(elem);
+                }
+            })
+            return pom(x + 1);
+        }
+        else return 0;
     }
+    pom(0);
 }
 
 losowo(5, 3, 10);
 
 // 7.
+const pierwsze10 = lodash.take(lp3, 10);
+
+lodash.forEach(pierwsze10, elem => {
+    lodash.delay(text => console.log(text), 2000, elem);
+})
 
 // 8.
 
 const spadek = lodash.filter(lp3, elem => elem.change < 0);
 
 console.log(spadek);
+
+// 9.
+
+const slownik = lodash.reduce(lp3, (acc, elem) => {
+    acc[elem.song] = elem;
+    return acc;
+}, {})
+
+console.log(slownik);
+
+// 10.
+const tablica = lodash.reduce(lp3, (acc, elem) => {
+    acc[elem.author]=[];
+    return acc;
+}, []);
+
+const tablicaUtwory = lodash.reduce(lp3, (acc, elem) => {
+    acc[elem.author].push({ 'song': elem.song, 'place': elem.place});
+    return acc;
+}, tablica);
+
+console.log(tablicaUtwory);
+
+// 11.
+
+const wystapienia = lodash.countBy(lp3, (elem) => elem.author);
+
+console.log(wystapienia);
+
+// 12.
+const spadekWzrost = lodash.reduce(lp3, ([spadek, wzrost], elem) => {
+    if (elem.change > wzrost) {
+        return [spadek, elem.change];
+    }
+    else if (elem.change < spadek) {
+        return [elem.change, wzrost];
+    }
+    else return [spadek, wzrost];
+}, [0, 0]);
+
+console.log(spadekWzrost);
