a03b4768af712efeedf76141bea85a60518f7067
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-05-30 18:49:39 +0200

    Laboratoria 12 - zadanie 2

--- lab12/zadanie2/src/promises.js
+++ lab12/zadanie2/src/promises.js
@@ -4,17 +4,19 @@
 // Stwórz obiekt klasy Promise -> niech zakończy się powodzeniem (resolve) po 5 sekundach i zwróci string 'Udało się!'. 
 // Jako callback niech wypisze zwrócony string w konsoli.
 
-const obietnica = new Promise((resolve, reject) => {
+const promise1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        () => "Udało się!"
-    }, 5000);
-})
-
-obietnica.then(response => console.log(response))
+      reject("Porażka");
+    }, 100);
+  });
+  
+promise1.then(value => console.log(value)).catch(error => console.log(error));
 
 // Zadanie 2.2.
 // Zmodyfikuj powyższy kod tak, aby zamiast z sukcesem - promise zakończył się porażką i zwracał string 'Porażka'.
 // Skorzystaj z then() aby obsłużyć błąd.
+
+
 
 // Zadanie 2.3.
 // Zamiast then(), zmodyfikuj powyższy kod używając catch()
@@ -22,3 +24,11 @@ obietnica.then(response => console.log(response))
 // Zadanie 2.4. 
 // Napisz funkcję multiplyAsync(x,y), która zwraca obiekt klasy Promise, kończący się porażką, gdy któryś za argumentów jest niepoprawny (nie jest liczbą).
 // W przeciwnym przypadku zwraca iloczyn dwóch liczb. Napisz callback, który wypisuje wynik w konsoli.
+
+function multiplyAsync(x, y) {
+    return new Promise((resolve, reject) => {
+        typeof x === 'number' && typeof y === 'number' ? resolve(x * y) : reject("Argumenty niepoprawne")
+    })
+}
+
+multiplyAsync(5, "tak").then(value => console.log(value)).catch(error => console.log(error))
